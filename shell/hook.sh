8aee2642d1c48767d13e5b8da2de85b82284385c
Jonas <palmuu@gmail.com>
2020-11-24 21:08:46 +0100

    Fetch direnv locations by hooking into direnv_layout_dir()
    Since one direnv allow could potentially create multiple direnvs we
    hook into direnv_layout_dir and stores every call result in a temp
    file which we use later to link with.
    
    This also fixes the issue where direnv allow is run from subfolders.
    
    Fixes #2.

--- shell/hook.sh
+++ shell/hook.sh
@@ -4,11 +4,26 @@ mkdir -p $direnv_store_dir
 # store current source_env as _source_env
 eval "$(declare -f source_env | sed '1s/.*/_&/')"
 
-source_env() {
-	_source_env $@
+# store current direnv_layout_path as _direnv_layout_path
+eval "$(declare -f direnv_layout_dir | sed '1s/.*/_&/')"
 
+direnv_gc_temp_file=$(mktemp)
+
+# direnv_layout_dir could be called multiple times when running
+# direnv allow and can also be called from subshells etc so we use
+# a temp file to store direnv locations we can update the links later
+#
+# it's important that this happens last in the direnvrc because the
+# user might override it
+direnv_layout_dir() {
+	local dir=$(_direnv_layout_dir $@)
+	echo $dir >>$direnv_gc_temp_file
+	echo $dir
+}
+
+direnv_gc_link() {
 	# create a symlink to the direnv layout dir inside the store
-	direnv_real_layout_dir=$(realpath $(direnv_layout_dir))
+	direnv_real_layout_dir=$(realpath $@)
 	if [[ -d "$direnv_real_layout_dir" ]]; then
 		direnv_layout_dir_hash=$(echo -n $direnv_real_layout_dir | sha256sum | cut -d ' ' -f 1)
 		direnv_store_path=$direnv_store_dir/$direnv_layout_dir_hash
@@ -18,4 +33,16 @@ source_env() {
 		ln -sf $direnv_real_layout_dir $direnv_store_path
 		touch -mh $direnv_store_path
 	fi
+}
+
+source_env() {
+	# restore old source env, this is function to be called once, and
+	# not from any child/parent direnvs (i.e. using source_up)
+	eval "$(declare -f _source_env | sed '1s/_source_env/source_env/')"
+	source_env $@
+
+	# link unique direnvs to the store
+	sort -u $direnv_gc_temp_file | while read line; do
+		direnv_gc_link $line
+	done
 }
